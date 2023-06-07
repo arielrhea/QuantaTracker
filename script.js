8b025e84cd91262f541ddc92bba895df865ca5a3
arielrhea <arielrhea@hotmail.com>
2023-06-07 18:32:23 -0500

    Updates on the different files
    Exchanges button is not ready and dark mode is being implemented

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const coinListContainer = document.getElementById('coin-list');
 const paginationContainer = document.getElementById('pagination');
 let currentPage = 1;
 
+
 // Fetch the list of coins from the API
 fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d&locale=en')
   .then(response => response.json())
